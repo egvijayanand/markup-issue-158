5a5e3332591b2bde7cd4cce4d0856c61156994dc
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-11-29 04:47:10 +0530

    https://github.com/CommunityToolkit/Maui.Markup/issues/158

---
+++ src/MauiApp6/TestLib/HomePage.cs
@@ -0,0 +1,19 @@
+﻿namespace TestLib
+{
+	public class HomePage : ContentPage
+	{
+		public HomePage()
+		{
+			Content = new Grid
+			{
+				Children =
+				{
+					new Label
+					{
+						Text = "Welcome to .NET MAUI!!!"
+					}.Center()
+				}
+			};
+		}
+	}
+}
